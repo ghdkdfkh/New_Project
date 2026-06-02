1534f3782fe32ce56ad1567897b1e07bd4f11d70
daas <filip.mso83@mail.ru>
2026-06-02 14:36:32 +0300

    Сделал health bar

---
+++ New_project/HealthBarUI/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthBarUI
+{
+    //4.2) ДЗ: UIElement
+    //Разработайте функцию, которая рисует некий бар(Healthbar, Manabar) в определённой позиции.
+    //Функция принимает некий закрашенный процент, длину бара и при необходимости дополнительные параметры.
+    //При 40% бар выглядит так:  [####______] 
+
+    //Реализуйте показ данных здоровья и маны.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = 5, maxHealth = 10;
+            int mana = 8, maxMana = 10;
+            while (true)
+            {
+                DrawBar(health, maxHealth, ConsoleColor.DarkGreen, 0, '#');
+                DrawBar(mana, maxMana, ConsoleColor.DarkBlue, 1, '^');
+
+                Console.SetCursorPosition(0, 4);
+                Console.Write("Введите количество здоровья ");
+                health += Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите количество маны ");
+                mana += Convert.ToInt32(Console.ReadLine());
+
+                Console.Clear();
+            }
+        }
+        static void DrawBar(int value, int maxValue, ConsoleColor color, int position, char symbol)
+        {
+            string bar = "";
+            for (int i = 0; i < value; i++)
+            {
+                bar += symbol;
+            }
+            Console.SetCursorPosition(0, position);
+            Console.Write('[');
+            Console.BackgroundColor = color;
+            Console.Write(bar);
+            Console.ResetColor();
+
+            bar = "";
+
+            for (int i = value; i < maxValue; i++)
+            {
+                bar += " ";
+            }
+
+            Console.Write(bar + ']');
+        }
+    }
+}
---
+++ New_project/HealthBarUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("HealthBarUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HealthBarUI")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("aa6034fe-aea5-404b-bdfa-97d05298616c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
