fa0c01a85e61d03ab716181faa007e074df767ce
daas <filip.mso83@mail.ru>
2026-06-04 12:57:58 +0300

    Сделал программу на вычисление максимального элемента массива

---
+++ New_project/massivi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace massivi
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] array = new int[10];
+            int maxElement = int.MinValue;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 100);
+                Console.WriteLine("Элемент массива - " + array[i]);
+                if (maxElement < array[i])
+                {
+                    maxElement = array[i];
+                }
+            }
+            Console.WriteLine("максимальный элемент массива - " + maxElement);
+        }
+    }
+}
