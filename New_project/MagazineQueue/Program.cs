b310733d2a501964054719c8e1aa689f5d33fe75
daas <filip.mso83@mail.ru>
2026-06-08 10:01:31 +0300

    Сделал Толковый словарь

--- New_project/MagazineQueue/Program.cs
+++ New_project/MagazineQueue/Program.cs
@@ -10,6 +10,30 @@ namespace MagazineQueue
     {
         static void Main(string[] args)
         {
+            string userInput;
+
+            Dictionary<string, string> tolkoviySlovar = new Dictionary<string, string>();
+            tolkoviySlovar.Add("Приветсвие", "Действие при встречи с человеком");
+            tolkoviySlovar.Add("Сон", "Биологическая потребность");
+            tolkoviySlovar.Add("Москва", "Столица Российской Федерации");
+            foreach (var slovar in tolkoviySlovar)
+            {
+                Console.WriteLine($"Слово {slovar.Key}");
+            }
+            userInput = Console.ReadLine();
+            if (tolkoviySlovar.ContainsKey(userInput))
+            {
+                foreach (var slovar in tolkoviySlovar)
+                {
+                    Console.WriteLine($"Слово {slovar.Key} - Значение {slovar.Value}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Было введено неверно слово");
+            }
+            
+
         }
     }
 }
